a7c6c7938eadc0b4e03bed72e3a1de711780beb5
leejiwon6315 <leejiwon6315@naver.com>
2021-08-19 18:27:15 +0900

    feat: 애니메이션 추가

--- apps/main/src/domains/auth/signIn/SignIn.view.tsx
+++ apps/main/src/domains/auth/signIn/SignIn.view.tsx
@@ -11,12 +11,28 @@ import { SignInViewModel } from './SignIn.view.model';
 
 export const SignInView: React.VFC<SignInViewModel> = React.memo(() => {
   const [visiblePassword, setVisiblePassword] = useState<boolean>(false);
+  const [signInValue, setSignInValue] = useState<{ email: string; password: string }>({
+    email: '',
+    password: '',
+  });
+
+  const onChangeSignInValue = (e: React.ChangeEvent<any>) => {
+    const { name, value } = e.target;
+    setSignInValue({
+      ...signInValue,
+      [name]: value,
+    });
+  };
+
+  const isValidButton = () => {
+    return signInValue.email.length > 0 && signInValue.password.length > 0;
+  };
 
   const togglePassword = () => {
     setVisiblePassword((value) => !value);
   };
 
-  const signIn = (e: React.MouseEvent) => {
+  const signIn = (e: React.FormEvent) => {
     e.preventDefault();
   };
 
@@ -24,11 +40,19 @@ export const SignInView: React.VFC<SignInViewModel> = React.memo(() => {
     <AuthLayout>
       <Container>
         <Title>우리의 두어가 되어주세요 :)</Title>
-        <SignInForm>
+        <SignInForm onSubmit={signIn}>
           <label htmlFor="email">
             <span>이메일</span>
             <div>
-              <input id="email" type="text" name="email" placeholder="이메일을 입력해 주세요" />
+              <input
+                id="email"
+                type="text"
+                name="email"
+                placeholder="이메일을 입력해 주세요"
+                value={signInValue.email}
+                onChange={onChangeSignInValue}
+              />
+              <div className="animate_div" />
             </div>
           </label>
 
@@ -36,19 +60,22 @@ export const SignInView: React.VFC<SignInViewModel> = React.memo(() => {
             <span>비밀번호</span>
             <div>
               <input
-                type={visiblePassword ? 'text' : 'password'}
                 id="password"
+                type={visiblePassword ? 'text' : 'password'}
                 name="password"
                 placeholder="비밀번호를 입력해 주세요"
+                value={signInValue.password}
+                onChange={onChangeSignInValue}
               />
 
               <EyeButton onClick={togglePassword}>
                 <Image src={visiblePassword ? eye : blindEye} alt="visible password" />
               </EyeButton>
+              <div className="animate_div" />
             </div>
           </label>
 
-          <Button onClick={signIn}>로그인</Button>
+          <Button available={isValidButton()}>로그인</Button>
         </SignInForm>
         <Link href="/">
           <FindPasswordButton> 비밀번호를 잊어버렸나요? </FindPasswordButton>
@@ -58,7 +85,7 @@ export const SignInView: React.VFC<SignInViewModel> = React.memo(() => {
 
         <Title>DOOOER가 처음이신가요?</Title>
         <Link href="/">
-          <Button>회원 가입</Button>
+          <Button available>회원 가입</Button>
         </Link>
       </Container>
     </AuthLayout>
@@ -71,16 +98,16 @@ const Container = styled.div`
   flex-direction: column;
   flex: 1;
   padding-top: ${({ theme }) => theme.space.small};
+  color: ${({ theme }) => theme.color.black0};
 `;
 
 const Title = styled.h1`
   margin: ${({ theme }) => theme.space.large} 0 ${({ theme }) => theme.space.medium} 0;
   padding: ${({ theme }) => theme.space.small} 0 ${({ theme }) => theme.space.xxxSmall} 0;
   font-size: ${({ theme }) => theme.fontSize.title};
-  color: ${({ theme }) => theme.color.black0};
 `;
 
-const SignInForm = styled.div`
+const SignInForm = styled.form`
   margin: ${({ theme }) => theme.space.small} 0;
 
   label {
@@ -94,8 +121,20 @@ const SignInForm = styled.div`
     }
 
     div {
+      .animate_div {
+        margin: -1px;
+        padding: 0;
+        position: relative;
+        width: 0px;
+        height: 2px;
+        left: 43%;
+        background-color: ${({ theme }) => theme.color.blue};
+        transition: 0.3s ease all;
+        visibility: hidden;
+      }
+
       input {
-        width: 85%;
+        width: 86%;
         max-width: 442px;
         border: none;
         border-bottom: 1px solid ${({ theme }) => theme.color.black20};
@@ -104,6 +143,13 @@ const SignInForm = styled.div`
 
         &:focus {
           outline: none;
+
+          ~ .animate_div {
+            width: 86%;
+            max-width: 442px;
+            left: 0;
+            visibility: visible;
+          }
 
           &::placeholder {
             color: transparent;
@@ -114,13 +160,13 @@ const SignInForm = styled.div`
   }
 `;
 
-const Button = styled.button`
+const Button = styled.button<{ available: boolean }>`
   width: 100%;
   height: 64px;
   color: ${({ theme }) => theme.color.white};
   font-size: ${({ theme }) => theme.fontSize.large};
   border-radius: 6px;
-  background-color: ${({ theme }) => theme.color.blue};
+  background-color: ${(props) => (props.available === true ? props.theme.color.blue : props.theme.color.black20)};
 `;
 
 const FindPasswordButton = styled.button`
