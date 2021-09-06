95699f5accfbcc308b4f2a3643f50155941d8d2f
EDEN <leejiwon6315@naver.com>
2021-09-06 09:53:49 +0900

    feat: init files

---
+++ apps/main/src/domains/project/Project.view.model.ts
@@ -0,0 +1,7 @@
+import { ViewModel } from '~/core/ViewModel';
+
+export type ProjectViewModel = ViewModel<typeof useProjectViewModel>;
+
+export function useProjectViewModel() {
+  return {};
+}
---
+++ apps/main/src/domains/project/Project.view.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import { ProjectViewModel } from './Project.view.model';
+
+export const ProjectView: React.VFC<ProjectViewModel> = React.memo(() => {
+  return <></>;
+});
